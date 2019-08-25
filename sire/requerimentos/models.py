e1c943d31cc6ff40fc13ebf1612836438fce9f62
Rodrigo Lira <rodrigo.lira@paulista.ifpe.edu.br>
2019-08-25 20:56:14 -0300

    Adicionando arquivos iniciais

---
+++ sire/requerimentos/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Requerimento(models.Model):
+    nome_requisitante = models.CharField(max_lenght=200)
+    email = models.EmailField()
+    datetime_criacao = models.DateTimeField(auto_now_add=True)
+    datetime_edicao = models.DateTimeField(auto_now = True)
+
+class Anexo(models.Model):
+    datetime_criacao = models.DateTimeField(auto_now_add = True)
+    datetime_edicao = models.DateTimeField(auto_now = True)
+    requerimento = models.ForeignKey(Requerimento, on_delete = models.CASCADE) #Rever CASCADE
+    upload = models.FileField(upload_to='uploads/%Y/%m/%d/')
+
+class Despacho(models.Model):
+    requerimento = models.ForeignKey(Requerimento, on_delete=models.CASCADE) #Rever CASCADE
+    conteudo = models.CharField(max_lenght=2000)
+    datetime_criacao = models.DateTimeField(auto_now_add = True)
+    datetime_edicao = models.DateTimeField(auto_now = True)
