a745f622d8632f911346265454316f8c81bf1b09
Rodrigo Lira <rodrigo.lira@paulista.ifpe.edu.br>
2019-09-03 00:02:47 -0300

    adicionando novos models

--- sire/requerimentos/admin.py
+++ sire/requerimentos/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import Requerimento
 from .models import Anexo
 from .models import Despacho
+from .models import TipoRequerimento
+from .models import Curso
 
 admin.site.register(Requerimento)
 admin.site.register(Anexo)
 admin.site.register(Despacho)
+admin.site.register(Curso)
+admin.site.register(TipoRequerimento)
--- sire/requerimentos/models.py
+++ sire/requerimentos/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 
+class Curso(models.Model):
+    nome = models.CharField(max_length= 150)
+
+class TipoRequerimento(models.Model):
+    tipo = models.CharField(max_length= 150)
+
 class Requerimento(models.Model):
     nome_requisitante = models.CharField(max_length=200)
+    matricula = models.CharField(max_length=100)
+    tipo = models.ForeignKey(TipoRequerimento, on_delete = models.SET_NULL, null = True)
     email = models.EmailField()
     datetime_criacao = models.DateTimeField(auto_now_add=True)
     datetime_edicao = models.DateTimeField(auto_now = True)
+    curso = models.ForeignKey(Curso, on_delete = models.SET_NULL, null = True)
 
 class Anexo(models.Model):
     datetime_criacao = models.DateTimeField(auto_now_add = True)
     datetime_edicao = models.DateTimeField(auto_now = True)
     requerimento = models.ForeignKey(Requerimento, on_delete = models.CASCADE) #Rever CASCADE
+    descricao = models.CharField(max_length = 400)
     upload = models.FileField(upload_to='uploads/%Y/%m/%d/')
 
 class Despacho(models.Model):
     requerimento = models.ForeignKey(Requerimento, on_delete=models.CASCADE) #Rever CASCADE
-    conteudo = models.CharField(max_length=2000)
+    conteudo = models.CharField(max_length=5000)
     datetime_criacao = models.DateTimeField(auto_now_add = True)
     datetime_edicao = models.DateTimeField(auto_now = True)
