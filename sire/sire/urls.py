c513a0ad8a66a0c19b657491ce5cc22be676a6a2
Rodrigo Lira <rodrigo.lira@paulista.ifpe.edu.br>
2019-09-05 12:39:40 -0300

    Adicionei uma string vazia

--- sire/sire/urls.py
+++ sire/sire/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView 
 
+" "
 
 urlpatterns = [
     path('requerimento/', include('requerimentos.urls')),
